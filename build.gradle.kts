c2b7fdeceeaf95b760288fb9467800a5fd5e04c9
Rtur2003 <hasannarthurrr@gmail.com>
2025-12-08 00:43:40 +0300

    build: align toolchain and sdk versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,8 +4,8 @@ plugins {
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.compose) apply false
     //Hilt
-    id("com.google.dagger.hilt.android") version "2.57.2" apply false
-    //KSP
-    id("com.google.devtools.ksp") version "2.2.20-2.0.3" apply false
+    id("com.google.dagger.hilt.android") version "2.51.1" apply false
+    //KSP aligned to Kotlin 1.9.24
+    id("com.google.devtools.ksp") version "1.9.24-1.0.20" apply false
     alias(libs.plugins.google.gms.google.services) apply false
 }
